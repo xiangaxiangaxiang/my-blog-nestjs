0161be0ea4f32aefa5d7490bb339cdff1fc40a89
xiangaxiangaxiang <42111444+xiangaxiangaxiang@users.noreply.github.com>
2021-05-25 11:37:36 +0800

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,6 @@
-const baseStaticPath = process.env.NODE_ENV === 'production' ?
-    '/opt/static' :
-    'E:/my-project/static'
-
 export default {
     jwtSecret: 'lalalademaxiya',
     jwtexpiresIn: `${60*60*24}s`,
-    baseStaticPath,
-    adminSecret: 'lalalademaxiyawansui!!!'
+    baseStaticPath: 'path',
+    adminSecret: 'lalalademaxiya'
 }
